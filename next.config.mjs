7650dcaf1b36d7325c176d832d19b2519b233856
cilasmiguel-git <miguelcilascolacobezerramiguel@gmail.com>
2026-04-13 12:30:37 -0300

    ci: configure GitHub Pages deployment and Next.js static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/projeto_redencao_site',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
